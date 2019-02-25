ed45950f486a9e48c5b1e02141782f766488f572
cflow <christian@flodihn.se>
2019-02-25 18:07:02 +0100

    Added skirmish server.

---
+++ skirmish_srv/ebin/skirmish_srv.app
@@ -0,0 +1,8 @@
+{application,skirmish_srv,
+             [{description,[]},
+              {vsn,"1"},
+              {registered,[]},
+              {applications,[kernel,stdlib]},
+              {mod,{skirmish_srv_app,[]}},
+              {env,[]},
+              {modules,[skirmish_srv,skirmish_srv_app,skirmish_srv_sup]}]}.
---
+++ skirmish_srv/src/skirmish_srv_app.erl
@@ -0,0 +1,20 @@
+-module(skirmish_srv_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/0, start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start() ->
+    application:start(skirmish_srv),
+    observer:start().
+
+start(_StartType, _StartArgs) ->
+    skirmish_srv_sup:start_link().
+
+stop(_State) ->
+    ok.
